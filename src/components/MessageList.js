017f84fe206cdf7eecd82367d136dc81ec45d8c0
Timjoo <timjoo@sk.com>
2020-02-28 09:55:40 +0900

    Basic message Mgmt

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,3 +1,20 @@
+import MessageListItem from './MessageListItem.js';
 export default {
-
+    name: 'MessageList',
+    Template:`<ul>
+        <message-list-item v-for="item in items" :item="item" :key="item.id"
+        @delete="deleteMessage(item)"></message-list-item>
+    </ul>`
+    ,props: {
+        items: {
+            type: Array,
+            required: true
+        }
+    }
+    ,components: { MessageListItem }
+    ,methods: {
+        deleteMessage(message) {
+            this.$emit('delete', message);
+        }
+    }
 }
